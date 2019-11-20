d4b08611a50d0b42f61d4db6d3577a53abd2e157
Muhammad Usman Majeed <12mm123@queensu.ca>
2019-11-20 17:37:04 -0500

    building page github

---
+++ precache-manifest.d0c427b9bc4d989db19081c781229f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0fe56fcf478070a70eb1a7df8c41e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c1d40427691cde0cc9b",
+    "url": "/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "d9a281917250c2e8c439",
+    "url": "/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "3c1d40427691cde0cc9b",
+    "url": "/static/js/2.c2f98c03.chunk.js"
+  },
+  {
+    "revision": "d9a281917250c2e8c439",
+    "url": "/static/js/main.c3620215.chunk.js"
+  },
+  {
+    "revision": "eb25a4c3457d2519ab3f",
+    "url": "/static/js/runtime-main.e120218d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
